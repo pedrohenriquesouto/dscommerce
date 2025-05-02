fe8c124abf22fad0bde166605c8bd2599311d307
Pedro Souto <pedrohrsouto@gmail.com>
2025-05-02 11:34:06 -0300

    OrderItem adicionado

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -3,6 +3,8 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -23,8 +25,10 @@ public class Order {
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
 
-    public Order() {
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
+    public Order() {
     }
 
     public Order(Long id, Instant moment, User client, OrderStatus status, Payment payment) {
